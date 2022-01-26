f543895e06e4f6aa0dac3f7d5b09ac36a21f0e92
Muhammad Nadeem <nd.maqbool@gmail.com>
2022-01-26 22:11:42 +0200

    component setup products page

---
+++ src/views/Products.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container mb-5">
+    <div class="row">
+      <div class="col">
+        <div class="row">
+          <Product v-for="(product, i) in inventory" :key="i" :product="product" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import food from '../food.json'
+import Product from '../components/Product.vue'
+export default {
+  name: 'Products',
+  data () {
+    return {
+      inventory: food,
+      cart: {}
+    }
+  },
+  components: {
+    Product
+  }
+}
+</script>
