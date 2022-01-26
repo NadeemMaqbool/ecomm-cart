59faf63e9a0c899f3cf11144593d101f82c0fcbb
Muhammad Nadeem <nd.maqbool@gmail.com>
2022-01-26 22:55:21 +0200

    component cart

--- src/main.js
+++ src/main.js
@@ -3,5 +3,13 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(router).mount('#app')
+library.add(faTrash)
+
+const app = createApp(App)
+app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
---
+++ src/views/Cart.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">E-COMMERCE CART</h1>
+    </div>
+  </section>
+
+  <div class="container mb-4">
+    <div class="row">
+      <div class="col-12">
+        <div class="table-responsive">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">Product</th>
+                <th scope="col" class="text-center">Quantity</th>
+                <th scope="col" class="text-right">Price</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td>Cucumber</td>
+                <td>2</td>
+                <td class="text-right">20.3</td>
+                <td class="text-right">
+                  <button class="btn btn-sm btn-danger">
+                    <font-awesome-icon icon="trash" />
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td><strong>Total</strong></td>
+                <td class="text-right"><strong>€</strong></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      cart: {}
+    }
+  }
+}
+</script>
